066868fd74fe00a9def5eeb1cbabc2609cbf8d68
Rafael Perea <rph@wayakna.com>
2022-12-02 07:59:56 -0600

    update pagina

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,49 @@ const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaProductos = document.querySelector('#lista-cursos');
 let articulosCarrito = [];
 
+let navbar = document.querySelector('.menu-nav-index');
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .menu-nav-index a');
+
+const mostrar = (elemento) => {
+    elemento.style.display = 'block';
+}
+const ocultar = (elemento) => {
+    elemento.style.display = 'none';
+}
+
+function divShow(Id) {
+
+    if(document.getElementById(Id).style.height==="0px"
+    || document.getElementById(Id).style.height ==''
+    ){
+    document.getElementById(Id).style.height = "100px";
+    } else{
+        document.getElementById(Id).style.height = "0px";      
+    }   
+    
+}
+
+window.onscroll=()=>{
+
+    navbar.classList.remove('active');
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 200;
+        let id = sec.getAttribute('id');
+    
+        if(top >= offset && top < offset + height){
+          navLinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('header .menu-nav-index a[href*='+id+']').classList.add('active');
+          });
+        };
+    
+      });
+}
+
 window.onload = () =>{
 
     //Cargamos el contenido del carrito
